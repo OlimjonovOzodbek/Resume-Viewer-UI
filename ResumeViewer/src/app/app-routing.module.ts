a8208e6869cb7f600c6c0ccd6c87f8b810bff7e2
UsmanHashimov <hashimovusman01@gmail.com>
2024-05-30 00:08:54 +0500

    guard added

--- ResumeViewer/src/app/app-routing.module.ts
+++ ResumeViewer/src/app/app-routing.module.ts
@@ -7,14 +7,15 @@ import { AllResumesComponent } from './pages/all-resumes/all-resumes.component';
 import { AllUserResumesComponent } from './pages/all-user-resumes/all-user-resumes.component';
 import { UploadResumeComponent } from './pages/upload-resume/upload-resume.component';
 import { HomeComponent } from './components/home/home.component';
+import { adminGuard, guestGuard, multiRoleGuard, superAdminGuard, userGuard } from './guard/role.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'all-users', component: AllUsersComponent },
-  { path: 'all-resumes', component: AllResumesComponent },
-  { path: 'my-resumes', component: AllUserResumesComponent },
-  { path: 'upload-resume', component: UploadResumeComponent },
+  { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
+  { path: 'all-users', component: AllUsersComponent, canActivate: [superAdminGuard] },
+  { path: 'all-resumes', component: AllResumesComponent, canActivate: [multiRoleGuard]},
+  { path: 'my-resumes', component: AllUserResumesComponent, canActivate: [userGuard] },
+  { path: 'upload-resume', component: UploadResumeComponent, canActivate: [userGuard] },
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', component: HomeComponent }
---
+++ ResumeViewer/src/app/guard/role.guard.ts
@@ -0,0 +1,75 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+
+var tokenKey = 'token';
+
+export const guestGuard: CanActivateFn = (route, state) => {
+  return true;
+}
+
+export const userGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  if (localStorage.getItem(tokenKey)?.length! > 0) {
+    const decodedToken: any = jwtDecode(localStorage.getItem(tokenKey)!)
+    const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if (role == 'User') {
+      return true;
+    }
+    router.navigate(['home'])
+    return false;
+  }
+  router.navigate(['login'])
+  return false;
+};
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  debugger;
+  
+  const router = inject(Router)
+  if (localStorage.getItem(tokenKey)?.length! > 0) {
+    const decodedToken: any = jwtDecode(localStorage.getItem(tokenKey)!)
+    const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if (role == 'Admin') {
+      return true;
+    }
+    router.navigate(['home'])
+    return false;
+  }
+  router.navigate(['login'])
+  return false;
+};
+
+export const superAdminGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  if (localStorage.getItem(tokenKey)?.length! > 0) {
+    const decodedToken: any = jwtDecode(localStorage.getItem(tokenKey)!)
+    const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if (role == 'SuperAdmin') {
+      return true;
+    }
+    router.navigate(['home'])
+    return false;
+  }
+  router.navigate(['login'])
+  return false;
+};
+
+export const multiRoleGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  if (localStorage.getItem(tokenKey)?.length! > 0) {
+    const decodedToken: any = jwtDecode(localStorage.getItem(tokenKey)!);
+    const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+    if (role == 'SuperAdmin' || role == 'Admin') {
+      return true;
+    }
+    router.navigate(['home'])
+    return false;
+  }
+  router.navigate(['login']);
+  return false;
+};
